48dc1a34535e2308efe82ecb647540ebff01f459
kobby08 <linusquam@gmail.com>
2022-02-14 02:33:05 +0000

    integrated database relationship with topics and blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,6 +4,9 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   friendly_id :title, use: :slugged
 
+  # associations
+  belongs_to :topic
+
   # validations
   validates_presence_of :title, :body
 end
