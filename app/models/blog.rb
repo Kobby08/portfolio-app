84afdb844960531a46efa863b83cefb4417e2646
kobby08 <linusquam@gmail.com>
2022-02-14 02:12:55 +0000

    implemented validations for blog, skill and portfolio

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,9 @@
 class Blog < ApplicationRecord
-  enum status: { draft: 0, published: 1 }
   extend FriendlyId
+
+  enum status: { draft: 0, published: 1 }
   friendly_id :title, use: :slugged
+
+  # validations
+  validates_presence_of :title, :body
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,4 @@
 class Portfolio < ApplicationRecord
+  # validations
+  validates_presence_of :title, :body, :main_image, :thumb_image
 end
